9fb419630fb732b9f4e7132e850b64b866c838a0
Thhinley Wangchuk <thinleywangchuk2097@gmail.com>
2024-05-25 16:57:05 +0600

    made the changes 4

--- src/main/java/app/com/java_jenkins/JavaJenkinsApplication.java
+++ src/main/java/app/com/java_jenkins/JavaJenkinsApplication.java
@@ -10,6 +10,8 @@ public class JavaJenkinsApplication {
 		System.out.println("Hello world1 ");
 		System.out.println("Hello world2 ");
 		System.out.println("Hello world3 ");
+		System.out.println("Hello world4 ");
+		System.out.println("Hello world5 ");
 		SpringApplication.run(JavaJenkinsApplication.class, args);
 	}
 
