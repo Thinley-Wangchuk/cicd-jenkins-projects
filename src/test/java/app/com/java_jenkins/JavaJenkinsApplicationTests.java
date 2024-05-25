4bea4c42452f4283f7271f4039a6586cbabf6e4a
Thhinley Wangchuk <thinleywangchuk2097@gmail.com>
2024-05-25 14:51:01 +0600

    first commit

---
+++ src/test/java/app/com/java_jenkins/JavaJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package app.com.java_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
