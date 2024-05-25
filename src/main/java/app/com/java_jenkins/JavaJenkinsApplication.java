2de0444b31c1a2cad523cf9fc53db76b609a43ea
Thhinley Wangchuk <thinleywangchuk2097@gmail.com>
2024-05-25 16:49:47 +0600

    made the changes for third times

--- src/main/java/app/com/java_jenkins/JavaJenkinsApplication.java
+++ src/main/java/app/com/java_jenkins/JavaJenkinsApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaJenkinsApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world ");
+		System.out.println("Hello world1 ");
+		System.out.println("Hello world2 ");
+		System.out.println("Hello world3 ");
 		SpringApplication.run(JavaJenkinsApplication.class, args);
 	}
 
