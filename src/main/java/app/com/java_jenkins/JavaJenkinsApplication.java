7890427634508ad1f83adc496f0d0ffca323f530
Thhinley Wangchuk <thinleywangchuk2097@gmail.com>
2024-05-28 15:13:19 +0600

    made the second changes

--- src/main/java/app/com/java_jenkins/JavaJenkinsApplication.java
+++ src/main/java/app/com/java_jenkins/JavaJenkinsApplication.java
@@ -11,8 +11,6 @@ public class JavaJenkinsApplication {
 		System.out.println("Hello world2 ");
 		System.out.println("Hello world3 ");
 		System.out.println("Hello world4332 ");
-		System.out.println("Hello world77 ");
-		System.out.println("Hello world76 ");
 		System.out.println("Hello world76 ");
 		System.out.println("Hello world999 ");
 		SpringApplication.run(JavaJenkinsApplication.class, args);
